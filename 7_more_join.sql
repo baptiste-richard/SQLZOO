cf06aa393857356aa9254bd8c95821190a7548ae
Baptiste Richard <baptiste.richard00@gmail.com>
2016-10-13 12:07:36 -0400

    Update 7_more_join.sql

--- 7_more_join.sql
+++ 7_more_join.sql
@@ -69,3 +69,10 @@ WHERE ord=1 AND title IN(SELECT title
 FROM (movie JOIN casting ON movie.id=movieid) JOIN actor ON actor.id=actorid
 WHERE name='Julie Andrews')
 ORDER BY name
+
+# 14. Obtain a list, in alphabetical order, of actors who've had at least 30 starring roles
+SELECT name FROM movie JOIN casting ON (movie.id=movieid)
+                       JOIN actor on (actor.id=actorid) 
+WHERE ord=1
+GROUP BY name
+HAVING COUNT(name) >= 30
